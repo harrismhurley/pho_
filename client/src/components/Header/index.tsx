5b73ab4f64a96d06f9e7a0dc1b32877712005afb
harrismhurley <harrismhurley@icloud.com>
2025-03-01 02:40:55 -0600

    basic outline and formatting

--- client/src/components/Header/index.tsx
+++ client/src/components/Header/index.tsx
@@ -1,10 +1,23 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+
 import './index.css';
 
 export default function Header() {
-    return (
-      <header>
-        <h1 className='site-header'>pho dat bit</h1>
-      </header>
-    )
+  return (
+    <header>
+
+      <div className='header_container'>
+        <nav className='header_nav'>
+          <ul>
+            <li><Link to="/" >Home</Link></li>
+            <li><Link to="/about" >About</Link></li>
+            <li><Link to="/register" >Register</Link></li>
+            <li><Link to="/gallery" >Gallery</Link></li>
+            <li><Link to="/contact" >Contact</Link></li>
+          </ul>
+        </nav>
+        <h1 className='header_text'>pho da</h1>
+      </div>
+    </header>
+  )
 }
