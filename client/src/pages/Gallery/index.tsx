6c5176b3cfb315821773197ba9e5d3a6e3b5d38b
harrismhurley <harrismhurley@icloud.com>
2025-03-02 02:53:34 -0600

    setup home and galley, grid needs to be responsive? prob not the right word

--- client/src/pages/Gallery/index.tsx
+++ client/src/pages/Gallery/index.tsx
@@ -1,9 +1,43 @@
 import './index.css';
+import GalleryItem from '../../components/GalleryItem';
 
-export default function Gallery() {
+const Gallery: React.FC = () => {
+  // placeholder photos 
+  const images = [
+    { id: 1, aspect: 'square' },
+    { id: 2, aspect: 'portrait' },
+    { id: 3, aspect: 'landscape' },
+    { id: 4, aspect: 'square' },
+    { id: 5, aspect: 'landscape' },
+    { id: 6, aspect: 'portrait' },
+    { id: 7, aspect: 'portrait' },
+    { id: 8, aspect: 'landscape' },
+    { id: 9, aspect: 'square' },
+    { id: 10, aspect: 'square' },
+    { id: 11, aspect: 'portrait' },
+    { id: 12, aspect: 'landscape' },
+    { id: 13, aspect: 'landscape' },
+    { id: 14, aspect: 'portrait' },
+    { id: 15, aspect: 'portrait' },
+    { id: 16, aspect: 'landscape' },
+    { id: 17, aspect: 'square' },
+    { id: 18, aspect: 'square' },
+    { id: 19, aspect: 'portrait' },
+  ];
+
+
   return (
-    <div>
-      <h1>Gallary</h1>
+    <div className="gallery_container">
+      <h1>Gallery</h1>
+      <br />
+      <div className="gallery_grid">
+        {images.map((image) => (
+          <GalleryItem key={image.id} aspect={image.aspect as 'square' | 'portrait' | 'landscape'} />
+        ))}
+      </div>
     </div>
   )
 }
+
+
+export default Gallery
--- client/src/pages/Home/index.tsx
+++ client/src/pages/Home/index.tsx
@@ -1,9 +1,30 @@
 import './index.css';
 
+
 export default function Home() {
-    return (
-      <div>
-        <h1>Home</h1>
+  return (
+    <div className="home_container">
+      <div className="leading_div">
+
+        <div className="grid_container">
+          <div className="grid_item"> </div>
+          <div className="grid_item"> </div>
+          <div className="grid_item"> </div>
+          <div className="grid_item"> </div>
+        </div>
       </div>
-    )
+
+
+
+      <div className="following_div">
+        <div className="widget_container">
+        </div>
+
+        <div className="text_container">
+          <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Illo ratione deleniti praesentium fugit nesciunt rerum laboriosam perferendis eius similique, accusamus accusantium, magni fugiat eligendi voluptatem maiores reprehenderit unde, dolorem voluptas.</p>
+        </div>
+      </div>
+
+    </div>
+  )
 }
